706365a13f746267590b865eee714bcb2ef1d45b
glendadrew <gadrew@ucdavis.edu>
2023-04-09 22:13:26 -0700

    animations on and off

---
+++ script.js
@@ -0,0 +1,55 @@
+(function () {
+    'use strict';
+    const images = ['images/star3.png', 'images/flower.png'];
+    const banners = ['images/courage3.png', 'images/beauty.png'];
+    const header = document.querySelector('header');
+    const image = document.querySelector('#image');
+    const banner = document.querySelector('#banner')
+    let counter = 0;
+
+    setInterval(changeImage, 4000);
+
+    header.className = 'fadeIn';
+    console.log('initial fade in of header');
+    // banner.className = 'scaleUpClass';
+    // console.log('initial scale up of banner');
+
+    function changeImage() {
+        console.log('changeImage called')
+        header.className = 'fadeOut';
+        // image.className = 'fadeOut';
+        // banner.className = 'fadeOut';
+        image.style.animation = 'spinOut 1s forwards';
+        banner.style.animation = 'scaleOut 1s forwards';
+       
+        // banner.
+        
+        setTimeout(function(){
+            
+            counter < images.length - 1 ? counter++ : counter = 0;
+            // need to scale down the image and the banner here
+            image.src = images[counter];
+            banner.src = banners[counter];
+            header.className = 'fadeIn';
+
+            setTimeout(function(){
+                // image.className = 'fadeIn';
+                // banner.className = 'fadeIn';
+                image.classList.add('spinInClass');
+                image.style.animation = 'spinIn 1s forwards';
+                
+                setTimeout(function(){
+                    banner.style.animation = 'scaleUp .5s forwards';
+                }, 500);
+            }, 250);
+        },1000)
+       
+    }
+    // This is a fix for height on iOS
+    // const appHeight = () => {
+    //     const doc = document.documentElement;
+    //     doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+    // }
+    // window.addEventListener('resize', appHeight);
+    // appHeight();
+})()
